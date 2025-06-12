f48a1840daacea03c6c6ff5b2e60f4d9f4bd738c
Juan Carlos Torres <juanperu2006@gmail.com>
2025-06-12 17:47:47 -0500

    Update RegistroInvitados.py 12-6-2025 JK

--- RegistroInvitados.py
+++ RegistroInvitados.py
@@ -144,13 +144,68 @@ date_entry.pack(pady=5)
 
 # Menú desplegable para elegir ubicación
 location_var = tk.StringVar()
-location_var.set("Lima")
+location_var.set("San Isidro")  # Valor por defecto
 location_label = tk.Label(step1_frame, text="Ubicación:", font=("Helvetica", 12))
 location_label.pack(pady=5)
-location_options = ["Lima", "Playa", "Chosica"]
+location_options = ["San Isidro", "Ricado Palma", "Asia","Restaurant","Peña","Bar","Invitado Academia"]
 location_menu = tk.OptionMenu(step1_frame, location_var, *location_options)
 location_menu.config(font=("Helvetica", 12))
 location_menu.pack(pady=5)
+
+# Label para mostrar el mensaje según la ubicación
+mensaje_ubicacion_label = tk.Label(step1_frame, text="", font=("Helvetica", 12), fg="blue", justify="left")
+mensaje_ubicacion_label.pack(pady=5)
+
+def actualizar_mensaje_ubicacion(*args):
+    if location_var.get() == "San Isidro":
+        mensaje_ubicacion_label.config(
+            text="INGRESO DE INVITADOS SEDE PRINCIPAL\n\n•De lunes a viernes\n\nIngreso máximo de 2 invitados sin costo."
+        )
+    elif location_var.get() == "Restaurant":
+        mensaje_ubicacion_label.config(
+            text="INGRESO DE INVITADOS RESTAURANT\n\n"
+                 "•De lunes a viernes\n"
+                 "Ingreso máximo de 8 invitados sin costo por día.\n\n"
+                 "•Sábado y Domingo\n"
+                 "Ingreso máximo de 4 invitados sin costo por día.\n\n"
+                 "S/. 5 por invitado adicional."
+        )
+    elif location_var.get() == "Peña":
+        mensaje_ubicacion_label.config(
+            text="INGRESO DE INVITADOS A LA PEÑA\n\n"
+                 "•viernes\n"
+                 "Ingreso máximo de 4 invitados sin costo.\n\n"
+                 "S/. 15 por invitado adicional."
+        )
+    elif location_var.get() == "Bar":
+        mensaje_ubicacion_label.config(
+            text="INGRESO DE INVITADOS RESTAL BAR\n\n"
+                 "•De lunes a Domingo\n"
+                 "Ingreso máximo de 4 invitados sin costo por día.\n\n"
+                 "S/. 10 por invitado adicional."
+        )
+    elif location_var.get() == "Asia":
+        mensaje_ubicacion_label.config(
+            text="INGRESO DE INVITADOS SEDE Asia\n\n"
+                 "•De lunes a Domingo\n\n"
+                 "Ingreso máximo de 4 invitados pagantes.\n\n"
+                 "S/. 50 Adultos / 12 o más años   o    S/25 Niños / de 3 a 11."
+        )
+    elif location_var.get() == "Invitado Academia":
+        mensaje_ubicacion_label.config(
+            text="INGRESO DE INVITADOS ACADEMIA\n\n"
+                 "se activa 30 minutos antes de academia"
+        )
+    else:
+        mensaje_ubicacion_label.config(text="INGRESO DE INVITADOS SEDE Ricado Palma\n\n"
+                 "•De lunes a Domingo\n\n"
+                 "S/. 30 Adultos / 12 o más años   o    S/10 Niños / De 3 a  11.")
+
+# Asociar la función al cambio de valor de location_var
+location_var.trace("w", actualizar_mensaje_ubicacion)
+
+# Mostrar el mensaje inicial si la ubicación es Lima
+actualizar_mensaje_ubicacion()
 
 button_continue = tk.Button(step1_frame, text="Continuar", font=("Helvetica", 12), command=go_to_step2)
 button_continue.pack(pady=20)
@@ -186,7 +241,7 @@ materno_label.pack(pady=5)
 entry_materno = tk.Entry(step2_frame, font=("Helvetica", 12))
 entry_materno.pack(pady=5)
 
-motivo_label = tk.Label(step2_frame, text="Motivo:", font=("Helvetica", 12))
+motivo_label = tk.Label(step2_frame, text="Observacion:", font=("Helvetica", 12))
 motivo_label.pack(pady=5)
 entry_motivo = tk.Entry(step2_frame, font=("Helvetica", 12))
 entry_motivo.pack(pady=5)
@@ -201,11 +256,18 @@ tipo_menu = tk.OptionMenu(step2_frame, tipo_var, *tipo_options)
 tipo_menu.config(font=("Helvetica", 12))
 tipo_menu.pack(pady=5)
 
-button_guardar = tk.Button(step2_frame, text="Guardar Invitado", font=("Helvetica", 12), command=guardar_invitado)
-button_guardar.pack(pady=20)
+# Botones para agregar invitado y pago online en la misma fila
+button_frame = tk.Frame(step2_frame)
+button_frame.pack(pady=20)
+
+button_guardar = tk.Button(button_frame, text="Agregar Invitado", font=("Helvetica", 12), command=guardar_invitado)
+button_guardar.pack(side="left", padx=10)
+
+button_pago_online = tk.Button(button_frame, text="Pago on line de invitados", font=("Helvetica", 12))
+button_pago_online.pack(side="left", padx=10)
 
 # Lista para mostrar los invitados registrados
-listbox_label = tk.Label(step2_frame, text="Invitados Registrados:", font=("Helvetica", 12))
+listbox_label = tk.Label(step2_frame, text="Invitados Agregados:", font=("Helvetica", 12))
 listbox_label.pack(pady=10)
 listbox = tk.Listbox(step2_frame, font=("Helvetica", 12), width=180, height=10)
 listbox.pack(pady=5)
