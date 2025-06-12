3494458d2b5986672592463db2e0cf929a2c544e
Juan Carlos Torres <juanperu2006@gmail.com>
2025-06-12 17:57:23 -0500

    Update entrenandoRF.py 12-6-25

--- entrenandoRF.py
+++ entrenandoRF.py
@@ -28,16 +28,16 @@ print('Número de etiquetas 0: ',np.count_nonzero(np.array(labels)==0))
 print('Número de etiquetas 1: ',np.count_nonzero(np.array(labels)==1))
 print('Número de etiquetas 2: ',np.count_nonzero(np.array(labels)==2))
 # Métodos para entrenar el reconocedor
-face_recognizer = cv2.face.EigenFaceRecognizer_create()
+#face_recognizer = cv2.face.EigenFaceRecognizer_create()
 #face_recognizer = cv2.face.FisherFaceRecognizer_create()
-#face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 # Entrenando el reconocedor de rostros
 print("Entrenando...")
 face_recognizer.train(facesData, np.array(labels))
 
 # Almacenando el modelo obtenido
-face_recognizer.write('modeloEigenFace.xml')
+#face_recognizer.write('modeloEigenFace.xml')
 #face_recognizer.write('modeloFisherFace.xml')
-#face_recognizer.write('modeloLBPHFace.xml')
+face_recognizer.write('modeloLBPHFace.xml')
 print("Modelo almacenado...")
