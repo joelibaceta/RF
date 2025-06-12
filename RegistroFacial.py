d88e6bf5b4747b9656dfa54a31149754505e688b
Juan Carlos Torres <juanperu2006@gmail.com>
2025-06-12 17:45:46 -0500

    Update RegistroFacial.py  12-6-25 JK

--- RegistroFacial.py
+++ RegistroFacial.py
@@ -4,44 +4,46 @@ import sqlite3
 import cv2
 import os
 import imutils
-entry_codigo = 3035
+entry_codigo = 3045
 
 def capturar_rostros(personName):
-	dataPath = 'C:\g0\VS\Data' #Cambia a la ruta donde hayas almacenado Data
-	personPath = dataPath + '/' + personName
+    dataPath = 'C:\g0\VS\Data' #Cambia a la ruta donde hayas almacenado Data
+    personPath = dataPath + '/' + personName
+    cantidadrostros = 300
+    count = 0
+    if not os.path.exists(personPath):
+        print('Carpeta creada: ',personPath)
+        os.makedirs(personPath)
+        cantidadrostros = 300
+        count = 0
+    cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    #cap = cv2.VideoCapture('Donald.mp4')  video
 
-	if not os.path.exists(personPath):
-		print('Carpeta creada: ',personPath)
-		os.makedirs(personPath)
+    faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+   
 
-	cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-	#cap = cv2.VideoCapture('Donald.mp4')  video
+    while True:
+        ret, frame = cap.read()
+        if ret == False: break
+        frame =  imutils.resize(frame, width=640)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        auxFrame = frame.copy()
+        faces = faceClassif.detectMultiScale(gray,1.3,5)
 
-	faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-	count = 0
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+            rostro = auxFrame[y:y+h,x:x+w]
+            rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
+            cv2.imwrite(personPath + '/rostro_{}.jpg'.format(count),rostro)
+            count = count + 1
+        cv2.imshow('frame',frame)
 
-	while True:
-		ret, frame = cap.read()
-		if ret == False: break
-		frame =  imutils.resize(frame, width=640)
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		auxFrame = frame.copy()
-		faces = faceClassif.detectMultiScale(gray,1.3,5)
+        k =  cv2.waitKey(1)
+        if k == 27 or count >= cantidadrostros:
+            break
 
-		for (x,y,w,h) in faces:
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-			rostro = auxFrame[y:y+h,x:x+w]
-			rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
-			cv2.imwrite(personPath + '/rostro_{}.jpg'.format(count),rostro)
-			count = count + 1
-		cv2.imshow('frame',frame)
-
-		k =  cv2.waitKey(1)
-		if k == 27 or count >= 300:
-			break
-
-	cap.release()
-	cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
 
 def buscar_datos():
     codigo = entry_codigo.get()
